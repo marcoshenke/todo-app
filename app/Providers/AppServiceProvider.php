81808f852d688570a0508272b65ff48cb7c140fe
@marcos.henke <marcos.henke@dponet.com.br>
2025-03-20 12:14:57 -0300

    deploy configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,22 +4,23 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+
     public function register(): void
     {
         $loader = \Illuminate\Foundation\AliasLoader::getInstance();
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+
+    public function boot(UrlGenerator $url): void
     {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+
         Route::middleware('api')
             ->prefix('api')
             ->group(base_path('routes/api.php'));
