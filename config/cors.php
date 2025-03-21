b1be491d508f8ba057a7beeacae25a6d1ab87328
@marcos.henke <marcos.henke@dponet.com.br>
2025-03-21 12:01:55 -0300

    cors ajuste

--- config/cors.php
+++ config/cors.php
@@ -27,8 +27,7 @@ return [
 
     'max_age' => 0,
 
-    // 'allowed_origins' => ['http://localhost:5173', 'https://todo-app-marcos-es-henke.vercel.app', 'https://todo-app-frontend-afditznrm-marcoshenkes-projects.vercel.app'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173', 'https://todo-app-marcos-es-henke.vercel.app', 'https://todo-app-frontend-afditznrm-marcoshenkes-projects.vercel.app'],
 
     'supports_credentials' => true,
 
